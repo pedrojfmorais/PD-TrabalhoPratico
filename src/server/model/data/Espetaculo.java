603bc8fa1e8430848ba9962b8aa257901d856e44
FranciscaDuarte <94937666+FranciscaDuarte@users.noreply.github.com>
2022-12-01 19:42:58 +0000

    Small changes to "Lugar" and "Espetaculo"

--- src/server/model/data/Espetaculo.java
+++ src/server/model/data/Espetaculo.java
@@ -1,14 +1,16 @@
 package server.model.data;
 
 import java.util.List;
+import java.util.Date;
 
 public class Espetaculo {
     int id;
-    boolean visibilidade;
+    boolean visibilidade = false;
     String designacao;
     String tipo;
-    String data;
-    String hora;
+    Date data;
+    /*String data;
+    String hora;*/
     int duracao;
     String local;
     String localidade;
@@ -16,13 +18,13 @@ public class Espetaculo {
     String classificacao;
     List<Lugar> lugares;
 
-    public Espetaculo(int id, boolean visibilidade, String designacao, String tipo, String data, String hora, int duracao, String local, String localidade, String pais, String classificacao, List<Lugar> lugares) {
+    public Espetaculo(int id, boolean visibilidade, String designacao, String tipo, Date data/*, String hora*/, int duracao, String local, String localidade, String pais, String classificacao, List<Lugar> lugares) {
         this.id = id;
         this.visibilidade = visibilidade;
         this.designacao = designacao;
         this.tipo = tipo;
         this.data = data;
-        this.hora = hora;
+        // this.hora = hora;
         this.duracao = duracao;
         this.local = local;
         this.localidade = localidade;
